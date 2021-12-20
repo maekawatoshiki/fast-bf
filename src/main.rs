1b63dad4f5a28b41e6a13f1affa8998209c5a7fa
maekawatoshiki <konndennsa@gmail.com>
2021-12-20 23:39:36 +0900

    Parse command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, path::PathBuf};
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "fbf", about = "Run brainf*ck faster")]
+struct Opt {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    let _code = fs::read_to_string(opt.path).expect("failed to load file");
 }
